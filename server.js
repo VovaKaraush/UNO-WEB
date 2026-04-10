626378fcbba687e5d333f26a4cfeaf32c9991724
Vova <starper2O0765@gmail.com>
2026-04-10 09:44:06 +0200

    rajout lancement serveur

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const path = require('path');
+const setupRoutes = require('./script/routage.js');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+// Initialize routes
+setupRoutes(app);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+});
