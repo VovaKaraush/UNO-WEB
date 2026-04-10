a8768c2c4ced59b6e84ee14a7cfa51989d959846
gunathan <nathan.gueroult@ynov.com>
2026-04-10 10:11:46 +0200

    add game.js

--- src/game.js
+++ src/game.js
@@ -1,42 +1,13 @@
-// uno cards (array of strings)
-const cards = [
-    {
-        color: "red",
-        value: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Skip", "Reverse", "Draw_Two", "Draw_Four", "Wild"]
-    },
-    {
-        color: "blue",
-        value: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Skip", "Reverse", "Draw_Two", "Draw_Four", "Wild"]
-    },
-    {
-        color: "green",
-        value: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Skip", "Reverse", "Draw_Two", "Draw_Four", "Wild"]
-    },
-    {
-        color: "yellow",
-        value: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Skip", "Reverse", "Draw_Two", "Draw_Four", "Wild"]
-    }]
+import { createDeck } from "./deck.js";
+import { createPlayers } from "./player.js";
 
-// players
-const players = [
-    {
-        name: "Player_1",
-        hand: []
-    },
-    {
-        name: "Player_2",
-        hand: []
-    }
-]
-
-// table
-const table = [
-    {
-        name: "Discard_Pile",
-        top: {} // exemple : { color: "red", value: "4"}
-    },
-    {
-        name: "Draw_Pile",
-        top: {}
-    }
-]
+export function createGame(players) {
+  return {
+    deck: createDeck().sort(() => Math.random() - 0.5),
+    discard: [],
+    players: createPlayers(players),
+    current_player: 0,
+    order: 1,
+    wild_color: "",
+  };
+}
